cc1e9c7c60f724f4e9a0178ecbaa003954a97f0e
Carter Janish <carter@patriotsigns.com>
2026-02-27 14:28:35 -0600

    fix: rename models from x_ prefix to ps. namespace - x_data_source -> ps.data.source - x_data_source_field -> ps.data.source.field - Fixes install error: x_ prefix conflicts with Odoo Studio custom models

--- modules/patriot_data_sources/models/data_source.py
+++ modules/patriot_data_sources/models/data_source.py
@@ -2,40 +2,40 @@ from odoo import models, fields
 
 
 class DataSource(models.Model):
-    _name = 'x_data_source'
+    _name = 'ps.data.source'
     _description = 'Data Source'
-    _order = 'x_name'
+    _order = 'name'
 
-    x_name = fields.Char('Name', required=True)
-    x_description = fields.Text('Description')
-    x_field_ids = fields.Many2many(
-        'x_data_source_field',
-        'x_data_source_field_rel',
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description')
+    field_ids = fields.Many2many(
+        'ps.data.source.field',
+        'ps_data_source_field_rel',
         'source_id',
         'field_id',
         string='Fields',
     )
-    x_field_count = fields.Integer(
+    field_count = fields.Integer(
         'Field Count',
         compute='_compute_field_count',
     )
 
     def _compute_field_count(self):
         for rec in self:
-            rec.x_field_count = len(rec.x_field_ids)
+            rec.field_count = len(rec.field_ids)
 
 
 class DataSourceField(models.Model):
-    _name = 'x_data_source_field'
+    _name = 'ps.data.source.field'
     _description = 'Data Source Field'
-    _order = 'x_name'
+    _order = 'name'
 
-    x_name = fields.Char('Field Name', required=True)
-    x_model_name = fields.Char('Model')
-    x_technical_name = fields.Char('Technical Name')
-    x_data_source_ids = fields.Many2many(
-        'x_data_source',
-        'x_data_source_field_rel',
+    name = fields.Char('Field Name', required=True)
+    model_name = fields.Char('Model')
+    technical_name = fields.Char('Technical Name')
+    data_source_ids = fields.Many2many(
+        'ps.data.source',
+        'ps_data_source_field_rel',
         'field_id',
         'source_id',
         string='Data Sources',
